8dc27befce8418ba146801ee3b589b904ff79bcd
davesauce <dave@cumulocreative.com>
2023-09-27 16:18:40 -0400

    fix: change extensions WIP

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,8 +6,10 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy", // Mock CSS modules
   },
-  setupFilesAfterEnv: ["./jest.setup.js"],
+  setupFilesAfterEnv: ["./jest.setup.cjs"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
 };

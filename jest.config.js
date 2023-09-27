8d1062834207726e17857fc6e63e76b1d8b3da26
davesauce <dave@cumulocreative.com>
2023-09-27 14:55:19 -0400

    failed jest fix attempt

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 // jest.config.js
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
+  // testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy", // Mock CSS modules
   },
   setupFilesAfterEnv: ["./jest.setup.js"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
